f96de1316a33aea20e94fbc18fb5b6e20ef077dd
AliasH315 <isma9iloff@yandex.ru>
2024-03-15 14:24:33 +0300

    Реализация в памяти интерфейса взаимодействия с данными студента

---
+++ src/main/java/ru/itone/iismagilov/study/spring/dao/impl/StudentDaoInMemoryImpl.java
@@ -0,0 +1,45 @@
+package ru.itone.iismagilov.study.spring.dao.impl;
+
+import ru.itone.iismagilov.study.spring.dao.StudentDao;
+import ru.itone.iismagilov.study.spring.dao.entity.StudentEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentDaoInMemoryImpl implements StudentDao {
+
+    private final Map<Integer, StudentEntity> idStudentEntityMap = new HashMap<>();
+
+    private int countID = 0;
+
+    @Override
+    public StudentEntity save(StudentEntity studentEntity) {
+        final int studentId = countID;
+        studentEntity.setId(studentId);
+        idStudentEntityMap.put(studentId, studentEntity);
+        countID = countID + 1;
+        return studentEntity;
+    }
+
+    @Override
+    public StudentEntity find(int id) {
+        return idStudentEntityMap.get(id);
+    }
+
+    @Override
+    public StudentEntity update(StudentEntity studentEntity) {
+        //получить объект из коллекции и проставить ему поля такие же как в аргументе метода
+        StudentEntity inMemoryStudentEntity = idStudentEntityMap.get(studentEntity.getId());
+        inMemoryStudentEntity.setName(studentEntity.getName());
+        inMemoryStudentEntity.setLastName(studentEntity.getLastName());
+        inMemoryStudentEntity.setMiddleName(studentEntity.getMiddleName());
+        inMemoryStudentEntity.setAge(studentEntity.getAge());
+        inMemoryStudentEntity.setPassport(studentEntity.getPassport());
+        return inMemoryStudentEntity;
+    }
+
+    @Override
+    public StudentEntity delete(int id) {
+        return idStudentEntityMap.remove(id);
+    }
+}
